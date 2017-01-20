8d0531e10281b36d62caa6498f8464989c4ba3c9
imenbr <imen.bradai.ib@gmail.com>
2017-01-20 22:45:00 +0100

    login done

--- template/verif.php
+++ template/verif.php
@@ -1,7 +1,7 @@
 <?php
  //session_start(); // Starting Session
 
-require_once('MySQLDatabase.php');
+//require_once('MySQLDatabase.php');
 //require_once('DecorClientManager.php');
 /*
 $postdata = file_get_contents("php://input");
@@ -10,8 +10,8 @@ $postdata = json_decode($postdata);
 
 //BDD
 
-$mySQLDatabase = new MySQLDatabase();
-$db = $mySQLDatabase->getConnection();
+//$mySQLDatabase = new MySQLDatabase();
+//$db = $mySQLDatabase->getConnection();
 
 //Manager
 //$decorClientManager = new DecorClientManager($db);
@@ -22,6 +22,8 @@ $db = $mySQLDatabase->getConnection();
 
 //if (isset($_POST['submit'])) {
    // echo "rania" ;
+
+if($db=mysqli_connect('localhost','root','','decoration')){
     if (empty($_POST['email2']) || empty($_POST['password2'])) {
         echo "Email or Password is invalid";
     }
@@ -35,13 +37,13 @@ $db = $mySQLDatabase->getConnection();
 // SQL query to fetch information of registerd users and finds user match.
         //$client = $decorClientManager->verifClient($email, $password);
 
-        $requette="SELECT * FROM client WHERE login='$login' AND password='$password'";
-        $result=mysqli_query($bdd,$requette);
+        $requette="SELECT * FROM client WHERE email='$email' AND password='$password'";
+        $result=mysqli_query($db,$requette);
         $nb=mysqli_num_rows($result);
 
         //echo "wsel5" ;
 
-        if ($nb==   1) {
+        if ($nb==1) {
            // $_SESSION['login_user']=$email; // Initializing Session
            //  $_SESSION['user_id']=$client->getId();
            header("location:index.php"); // Redirecting To Other Page
@@ -49,11 +51,12 @@ $db = $mySQLDatabase->getConnection();
        else {
             if($nb==0)
             {
-                $error = "Username or Password is invalid";
-                echo $error ;
+                //$error = "Username or Password is invalid";
+                //echo $error ;
+                echo "vous" ;
             }
 
 
     }
    // }
-} ?>
+} } ?>
